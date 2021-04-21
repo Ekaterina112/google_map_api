17cf14fb43df94c479467885029f1106c6a75cb6
Ekaterina <ekaterinan112@gmail.com>
2021-04-21 17:30:18 +0300

    I have no idea how to get paid key

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
 import './App.css';
+import {GoogleMap, useLoadScript} from '@react-google-maps/api';
+import mapStyles from './mapStyles'
+
+const libraries = ['places']
+const mapContainerStyle = {
+    width: '100vh',
+    height: '100vh'
+}
+const center = {
+    lat: 53.904541,
+    lng: 27.561523
+}
+const options = {
+    styles: mapStyles
+}
 
 function App() {
-  return (
-    <div className="App">
+    const {isLoaded, loadError} = useLoadScript({
+        googleMapsApiKey: "AIzaSyCkqA_uEBCpPYnvy4DQQ0HfDLYlBffsOFw",
+        libraries
+    })
 
-    </div>
-  );
+    if (loadError) return 'Error loading map'
+    if (!isLoaded) return 'Loading map'
+    return (
+        <div className="App">
+            <GoogleMap
+                mapContainerStyle={mapContainerStyle}
+                zoom={8}
+                options={options}
+                center={center}>
+
+            </GoogleMap>
+        </div>
+    );
 }
 
 export default App;
